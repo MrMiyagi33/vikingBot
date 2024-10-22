d8fea8d6f79dd27dc548a1e6f8baf0d778c8051a
Miyagi <97127664+MrMiyagi33@users.noreply.github.com>
2024-10-22 12:39:47 -0400

    Update runServer.sh

--- runServer.sh
+++ runServer.sh
@@ -8,7 +8,7 @@ prefix="$3"
 echo "prefix: "$prefix
 
 sed -i "s/BOT_TOKEN_HERE/$botCode/g" config.txt
-sed -i "s/URL/$url/g" config.txt
+sed -i "s/URL_PATH/$url/g" config.txt
 sed -i "s/PREFIX/$prefix/g" config.txt
 
 python3 viking.py
